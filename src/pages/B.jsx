8ccc9bea783acf057b9c98b70557b9eb541a835c
sarthak37 <sarthak27998@gmail.com>
2023-10-30 13:49:09 +0530

    poi

--- src/pages/B.jsx
+++ src/pages/B.jsx
@@ -7,11 +7,11 @@ const Banner = () => {
       <div className="content">
         <div className="label">MARKETPLACE</div>
         <div className="sliding-text">
-          <p style={{ color: 'green', fontSize: '7vh', fontweight: 'bold', paddingTop: '20px'}}>Email Templates</p>
-          <p style={{ color: 'green', fontSize: '7vh', fontweight: 'bold',paddingTop: '20px'}}>Email Sequences</p>
-          <p style={{ color: 'green', fontSize: '7vh',fontweight: 'bold', paddingTop: '20px'}}>Marketing Species</p>
+          <p style={{ color: 'green', fontSize: '6vh', fontweight: 'bold', paddingTop: '20px'}}>Email Templates</p>
+          <p style={{ color: 'green', fontSize: '6vh', fontweight: 'bold',paddingTop: '20px'}}>Email Sequences</p>
+          <p style={{ color: 'green', fontSize: '6vh',fontweight: 'bold', paddingTop: '20px'}}>Marketing Species</p>
         </div>
-        <p style={{  fontSize: '7vh' , paddingBottom: '20px', fontweight: 'bold'}}>by the community, for the community.</p>
+        <p style={{  fontSize: '6vh' , paddingBottom: '20px', fontweight: 'bold'}}>by the community, for the community.</p>
         <p style={{  fontSize: '3vh' }}>100s of free templates to help you craft the perfect marketing journey.</p>
       </div>
     </div>
